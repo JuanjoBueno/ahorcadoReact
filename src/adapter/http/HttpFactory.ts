af107b981df5c489047f9547b3bc43bbf16125e0
JuanjoBueno <jbaprogramacion@gmail.com>
2025-02-19 21:48:33 +0100

    errores

--- src/adapter/http/HttpFactory.ts
+++ src/adapter/http/HttpFactory.ts
@@ -1,9 +1,12 @@
 import {Config} from '../../config/Config';
+import {HttpAxios} from './HttpAxios';
 import {HttpFetch} from './HttpFetch';
 
 export class HttpFactory {
   static build() {
     switch (Config.wordAPI.type) {
+      case 'axios':
+        return new HttpAxios(Config.wordAPI);
       default:
         return new HttpFetch(Config.wordAPI);
     }
